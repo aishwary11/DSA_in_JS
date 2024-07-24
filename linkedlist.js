580500d1df141d24d3e4d7a43c58b046d0c42c32
Aishwary Shah <49820149+aishwary11@users.noreply.github.com>
2024-07-24 11:40:02 +0530

    linkedlist added

--- linkedlist.js
+++ linkedlist.js
@@ -38,13 +38,61 @@ class LinkedList {
     };
     return temp;
   }
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  shift() {
+    if (!this.head) return null;
+    let temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return temp;
+  }
+  getFirst() {
+    return this.head;
+  }
+  getLast() {
+    if (!this.head) return null;
+    let temp = this.head;
+    while (temp) {
+      if (!temp.next) {
+        return temp;
+      }
+      temp = temp.next;
+    }
+  }
+  get(index) {
+    let counter = 0;
+    let temp = this.head;
+    while (temp) {
+      if (counter === index) {
+        return temp;
+      }
+      counter++;
+      temp = temp.next;
+    }
+    return null;
+  }
 }
 
 const mylinkedList = new LinkedList(5);
+mylinkedList.unshift(1);
 mylinkedList.push(10);
 mylinkedList.push(15);
 console.log(mylinkedList);
 mylinkedList.pop();
-mylinkedList.pop();
-mylinkedList.pop();
+mylinkedList.shift();
 console.log(mylinkedList);
+console.log(mylinkedList.getFirst());
